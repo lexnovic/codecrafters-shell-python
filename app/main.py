2c7ee4922899f34dd5ab7766a596d19db1cf2e6d
Pavel Liakhnovich <lexnovic@gmail.com>
2025-10-26 11:02:10 +0100

    add exit

--- app/main.py
+++ app/main.py
@@ -7,11 +7,15 @@ def repl():
             command = input()
             if not command:
                 continue
+            if "exit" in command:
+                exit()
             print(f"{command}: command not found")
         except (EOFError, KeyboardInterrupt):
             print()
             break
 
+def exit():
+    sys.exit(0)
 
 
 if __name__ == "__main__":
