cc800f04d42e18d88192516d6e5db874b43959a7
Pavel Liakhnovich <lexnovic@gmail.com>
2025-10-26 11:31:23 +0100

    add type

--- app/main.py
+++ app/main.py
@@ -1,5 +1,7 @@
 import sys
 
+builtin_commands = ["exit", "echo", "type"]
+
 def repl():
     while True:
         try:
@@ -7,10 +9,12 @@ def repl():
             command = input()
             if not command:
                 continue
-            if "exit" in command:
+            elif "exit" in command:
                 do_exit()
             elif "echo" in command:
                 do_echo(command)
+            elif "type" in command:
+                do_type(command)
             else:
                 print(f"{command}: command not found")
         except (EOFError, KeyboardInterrupt):
@@ -26,6 +30,16 @@ def do_echo(command):
         print(" ".join(parts[1:]))
     else:
         print()
+
+def do_type(command):
+    parts = command.split()
+    if len(parts) == 1:
+        print("Specify command.")
+    else:
+        if parts[1] in builtin_commands:
+            print(f"{parts[1]} is a shell builtin")
+        else:
+            print(f"{parts[1]}: command not found")
 
 
 if __name__ == "__main__":
